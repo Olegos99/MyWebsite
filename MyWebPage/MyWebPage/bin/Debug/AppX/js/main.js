95c22b8211bc8555a425a2e69053fac8b01e40c7
Olegos99 <51860876+Olegos99@users.noreply.github.com>
2019-07-06 18:16:27 +0300

    Add files via upload

---
+++ MyWebPage/MyWebPage/bin/Debug/AppX/js/main.js
@@ -0,0 +1,29 @@
+﻿function DarkMode() {
+
+
+    var color = document.getElementsByTagName("h1")[0].style.color ;
+
+
+    if (color === "black") {
+        document.body.style.background = "#1e1e1e url('images/night.jpg') no-repeat right top"; 
+        document.getElementsByTagName("h1")[0].style.color = "#fff";
+        var pElements = document.getElementsByTagName("p");
+        for (var i = 0; i < pElements.length; i++) {
+            pElements[i].style.color = "#fff";
+        }
+    }
+    else {
+        document.body.style.background = "#1e1e1e url('images/day.jpg') no-repeat right top"; 
+        document.getElementsByTagName("h1")[0].style.color = "black";
+        var pElements = document.getElementsByTagName("p");
+        for (var i = 0; i < pElements.length; i++) {
+            pElements[i].style.color = "black";
+        }
+
+    }
+}
+
+
+
+
+
